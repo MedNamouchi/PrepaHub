a28e48f5070bb175e34cbc898c178de7bdaefab2
MedAmine <namouchi12345@gmail.com>
2023-06-15 14:02:23 +0100

    mazel ken searchbar

---
+++ search.php
@@ -0,0 +1,12 @@
+<?php
+if (isset($_GET['query'])) {
+  $searchQuery = $_GET['query'];
+
+  // Perform search operation with the $searchQuery
+  // Example: Query a database or search through data
+
+  // Display search results or redirect to a search results page
+  // Example: Print search results
+  echo "You searched for: " . $searchQuery;
+}
+?>
